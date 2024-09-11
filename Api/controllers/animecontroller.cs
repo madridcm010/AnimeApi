c92fa9c85d2450883b52ad8f8b70ef491b4d75f0
madridcm010 <snowfallslower@gmail.com>
2024-09-11 19:41:06 -0400

    lol

--- Api/controllers/animecontroller.cs
+++ Api/controllers/animecontroller.cs
@@ -18,42 +18,59 @@ namespace Api.controllers
             _context = context;
         }    
         [HttpGet]
-        public IActionResult GetAll(){
-            var anime = _context.Anime.ToList()
-                .Select(s => s.ToAnimeDto());
+        public async Task<IActionResult> GetAll(){
+            var anime = await _context.Anime.ToListAsync();
+            var animeDto = Anime.Select(s => s.ToAnimeDto());
             return Ok(anime);
         }
+
         [HttpGet("{id}")]
-        public IActionResult GetbyId([FromRoute]int id){
-            var anime = _context.Anime.Find(id);
+        public async Task<IActionResult> GetbyId([FromRoute]int id){
+            var anime = await_context.Anime.FindAsync(id);
             if(anime == null){
                 return NotFound();
             }
 
             return Ok(id);
         }
+
         [HttpPost]
-        public IActionResult Create([FromBody] CreateAnimeRequestDto animeDto){
+        public async Task<IActionResult> Create([FromBody] CreateAnimeRequestDto animeDto){
             var animemodel = animeDto.ToAnimeFromCreateDto();
-            _context.Anime.Add(animemodel);
-            _context.SaveChanges();
+            await _context.Anime.AddAsync(animemodel);
+            await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetbyId), new{id = animemodel.Id}, animemodel.ToAnimeDto());
         }
+
         [HttpPut]
         [Route("{id}")]
-        public IActionResult Update([FromRoute] int id,[FromBody] UpdateAnimeRequestDto updateDto){
-            var animemodel = _context.Anime.FirstorDefault(x=> x.Id == id);
+        public async Task<IActionResult> Update([FromRoute] int id,[FromBody] UpdateAnimeRequestDto updateDto){
+            var animemodel = await _context.Anime.FirstOrDefaultAsync(x => x.Id == id);
 
             if(animemodel == null){
                 return NotFound();
             }
-            animemodel.Id = updateDto.Id;
             animemodel.Name = updateDto.Name;
             animemodel.Author = updateDto.Author;
             animemodel.AniCompany = updateDto.AniCompany;
             animemodel.Genre = updateDto.Genre;
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return Ok(animemodel.ToAnimeDto());   
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id){
+            var animemodel =  await _context.Anime.FirstOrDefaultAsync(x => x.Id == id);
+            if(animemodel == null){
+                return NotFound();
+            }
+            _context.Anime.Remove(animemodel);
+           
+           await _context.SaveChangesAsync();
+
+            return NoContent();
+
         }
     }
 }
